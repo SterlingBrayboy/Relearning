e134b660c2eda0d7d2e44ca8ef1efa6e53206227
LEARNER <sterlingbrayboyn@gmail.com>
2026-03-05 23:59:20 -0500

    Working With Claude to Modify Function for Iterating Through Color Array + Explanation of Methodology

--- 1/javascript.js
+++ 1/javascript.js
@@ -1,17 +1,31 @@
-console.log('ROBOT SAYS: "Whagoin on?"');
-
 const colorBox = document.querySelector(".color_box");
-const colorRed = document.querySelector(".color_box-red");
-const colorAqua = document.querySelector(".color_box-aqua");
-const colorGreen = document.querySelector(".color_box-green");
-const colorPurple = document.querySelector(".color_box-purple");
+// const colorRed = document.querySelector(".color_box-red");
+// const colorAqua = document.querySelector(".color_box-aqua");
+// const colorGreen = document.querySelector(".color_box-green");
+// const colorPurple = document.querySelector(".color_box-purple");
 const button = document.getElementById("button");
 
 //This is a function to change colors of the color_box
 
+// message = ["STOP", "THE PAIN", "THIS IS HARRASSMENT", "911 HAS BEEN CALLED"];
+
+const colorArray = [
+  "color_box-red",
+  "color_box-aqua",
+  "color_box-green",
+  "color_box-purple",
+];
+
+// colorArray.forEach((color) => {
+//   colorBox.classList.toggle(color);
+// });
+
+let index = 0;
+
 function changeBox() {
-  colorBox.classList.toggle("color_box-red", true);
-  console.log("Nothin Much, Mayne");
+  colorBox.classList.remove(colorArray[index]);
+  index = (index + 1) % colorArray.length;
+  colorBox.classList.add(colorArray[index]);
 }
 
 // This is a function to execute changebox() when clicking the *CLICK ME* button
